12cb981d44988b711ccc746587b672d883e923a4
Katrina Pritchard <pritckat@gmail.com>
2018-09-27 18:30:15 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def add_artist?
-    if self.artist.present?
-      link_to self.artist.name, artist_path(self.artist)
+    if @song.artist.present?
+      link_to @song.artist.name, artist_path(@song.artist)
     else
-      link_to "Add Artist", edit_song_path(self)
+      link_to "Add Artist", edit_song_path(@song)
     end
   end
 end
