5485b962dac687e14d1d063ac10420c37539696d
Katrina Pritchard <pritckat@gmail.com>
2018-09-27 18:28:01 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def add_artist?
+    if @song.artist.present?
+      link_to @song.artist.name, artist_path(@song.artist)
+    else
+      <h3>link_to "Add Artist", edit_song_path(@song)</h3>
+    end
+  end
 end
